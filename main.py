f932181e8cb3cd60702b0d26eae716b2d8d1c769
Simo Rinne <maqqr@server.fake>
2013-07-25 20:16:30 +0300

    restructured code and added proper http handlers

--- main.py
+++ main.py
@@ -1,57 +1,105 @@
+# -*- coding: utf-8 -*-
+""" Captures images from Raspberry Pi webcam """
+
 import sys
+import time
 import pygame
 import pygame.camera
-import BaseHTTPServer
+import platform
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from optparse import OptionParser
-from socket import gethostbyname_ex, gethostname
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+#from socket import gethostbyname_ex, gethostname
 from threading import Thread
 
-parser = OptionParser()
-parser.add_option('-i', '--ip', dest='ip', help='Server IP address')
-
-options, args = parser.parse_args()
-
-if options.ip == None:
-  print 'Error: You must give the IP address.\nRun with -h to see usage.\n'
-  sys.exit(0)
-
-IP = options.ip
+# todo: get IP automatically
+#IP = ...
 PORT = 9001
+IMAGEFILE = 'image.png'
 
-HandlerClass = SimpleHTTPRequestHandler
-HandlerClass.protocol_version = "HTTP/1.0"
-ServerClass = BaseHTTPServer.HTTPServer
+class CameraThread(Thread):
+    """ CameraThread captures images from USB webcam """
 
-server = ServerClass((IP, PORT), HandlerClass)
-print "Webcam server running at", (IP,PORT)
-try:
-  server.serve_forever()
-except KeyboardInterrupt:
-  print '\nClosing server.'
-  server.socket.close()
+    def __init__(self, camera):
+        super(CameraThread, self).__init__()
+        self.camera = camera
+        self.capture = True
 
-sys.exit(0)
+    def stop_capture(self):
+        """ Stops camera capture """
+        self.capture = False
+        self.camera.stop()
+    
+    def run(self):
+        """ Camera capture main loop """
+        while self.capture:
+            image = self.camera.get_image()
+            image = pygame.transform.scale(image, (640, 480))
+            pygame.image.save(image, IMAGEFILE)
+            time.sleep(1.0)
 
-pygame.init()
-pygame.camera.init()
 
-image = pygame.Surface((640, 480))
+class HTTPHandler(BaseHTTPRequestHandler):
+    """ HTTPHandler sends webcam images over HTTP """
+    protocol_version = "HTTP/1.0"
+    filename = 'image.png'
+    
+    def do_GET(self):
+        """ Handle GET request """
+        print self.path
+        if self.path == '/webcam':
+            self.send_response(200)
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+            with open(IMAGEFILE, 'rb') as imgfile:
+                self.wfile.write(imgfile.read())
+        else:
+            self.send_error(404, "Path not found: %s" % self.path)
 
-cam_list = pygame.camera.list_cameras()
-print 'Camlist', cam_list
-if len(cam_list) == 0:
-  print 'No camera found.'
-  pygame.quit()
-  sys.exit(-1)
 
-webcam = pygame.camera.Camera(cam_list[0], (32,24))
-webcam.start()
+def main():
+    # Parse command-line options
+    parser = OptionParser()
+    parser.add_option('-i', '--ip', dest='ip', help='Server IP address')
+    options, _ = parser.parse_args()
+    if options.ip == None:
+        print 'Error: You must give the IP address.\nRun with -h to see usage.'
+        sys.exit(0)
 
-image = webcam.get_image()
-image = pygame.transform.scale(image, (640, 480))
-pygame.image.save(image, 'image.png')
+    ipaddr = options.ip
 
-webcam.stop()
-pygame.quit()
+    
+    # Initialize Pygame and camera
+    pygame.init()
+    pygame.camera.init()
 
+    cam_list = pygame.camera.list_cameras()
+    print 'Camlist', cam_list
+    if len(cam_list) == 0:
+        print 'No camera found.'
+        pygame.quit()
+        sys.exit(-1)
+
+    if platform.uname()[0] != 'Windows':
+        camera = pygame.camera.Camera(cam_list[0], (32, 24))
+        camera.start()
+
+        camthread = CameraThread(camera)
+        camthread.start()
+    else:
+        camthread = None
+
+    # Start server
+    server = HTTPServer((ipaddr, PORT), HTTPHandler)
+    print "Webcam server running at", (ipaddr, PORT)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '\nClosing server.'
+        server.socket.close()
+
+    if camthread:
+        camthread.stop_capture()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
